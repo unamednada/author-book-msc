14d118fcb5632d8ac497c68a684f55c25c151f61
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 19:44:05 -0300

    Create index.js app.get calling model author get all

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const { PORT } = process.env;
+
+const Author = require('./models/Author');
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+
+  res.status(200).json(authors);
+});
+
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
