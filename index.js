ee7644a9954028e6ba3a735e17a062e7880148f7
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 20:00:07 -0300

    Create getbyauthorid in books model and index.js appget

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/authors', async (_req, res) => {
   res.status(200).json(authors);
 });
 
+app.get('/books/:author_id', async (req, res) => {
+  const { author_id } = req.params;
+
+  const books = await Book.getByAuthorId(author_id);
+
+  res.status(200).json(books);
+});
+
 app.get('/books', async (_req, res) => {
   const books = await Book.getAll();
 
--- models/Book.js
+++ models/Book.js
@@ -2,6 +2,7 @@ const connection = require('./connection');
 
 const QUERIES = {
   getAll: 'SELECT id, title, author_id FROM books',
+  getByAuthorId: 'SELECT id, title FROM books WHERE author_id = ?'
 };
 
 const serialize = (bookData) => ({
@@ -16,6 +17,13 @@ const getAll = async () => {
   return books.map(serialize);
 };
 
+const getByAuthorId = async (authorId) => {
+  const [books] = await connection.execute(QUERIES.getByAuthorId, [authorId]);
+
+  return books.map(serialize);
+};
+
 module.exports = {
   getAll,
+  getByAuthorId,
 };
