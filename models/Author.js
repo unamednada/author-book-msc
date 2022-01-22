6c3f52d817a923dae7b92f87fb4f72bf40229dde
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 19:49:24 -0300

    Create serialize and format in model author

--- models/Author.js
+++ models/Author.js
@@ -4,10 +4,25 @@ const QUERIES = {
   getAll: 'SELECT id, first_name, middle_name, last_name FROM authors',
 }
 
+const format = ({ id, firstName, middleName, lastName }) => ({
+  id,
+  firstName,
+  middleName,
+  lastName,
+  fullName: [firstName, middleName, lastName].filter(exist => exist).join(' '),
+});
+
+const serialize = (authorData) => ({
+  id: authorData.id,
+  firstName: authorData.first_name,
+  middleName: authorData.middle_name,
+  lastName: authorData.last_name,
+});
+
 const getAll = async () => {
   const [authors] = await connection.execute(QUERIES.getAll);
 
-  return authors;
+  return authors.map(serialize).map(format);
 };
 
 module.exports = {
