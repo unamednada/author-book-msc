71c74b1c6d735f96c6bad5addc8a985fbb820a7a
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 19:53:48 -0300

    Create model for Book in author-book-msc

---
+++ models/Book.js
@@ -0,0 +1,21 @@
+const connection = require('./connection');
+
+const QUERIES = {
+  getAll: 'SELECT id, title, author_id FROM books',
+};
+
+const serialize = (bookData) => ({
+  id: bookData.id,
+  title: bookData.title,
+  authorId: bookData.author_id,
+});
+
+const getAll = async () => {
+  const [books] = connection.execute(QUERIES.getAll);
+
+  return books.map(serialize);
+};
+
+module.exports = {
+  getAll,
+};
