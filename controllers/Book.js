1546b8351cc190166f6489300268e9278954406f
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 22:01:33 -0300

    Add controller to book. queries ok

---
+++ controllers/Book.js
@@ -0,0 +1,43 @@
+const Book = require('../services/Book');
+
+const getAll = async (_req, res) => {
+  const books = await Book.getAll();
+
+  res.status(200).json(books);
+};
+
+const getByAuthorId = async (req, res) => {
+  const { author_id } = req.params;
+
+  const books = await Book.getByAuthorId(author_id);
+
+  if (!books) return res.status(404).json({ message: 'Books Notfound' });
+
+  res.status(200).json(books);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+
+  const book = await Book.findById(id);
+
+  if (!book) return res.status(404).json({ message: 'Book Notfound' });
+
+  res.status(200).json(book);
+};
+
+const create = async (req, res) => {
+  const { title, author_id } = req.body;
+  const book = await Book.create(title, author_id);
+
+  if (!book) return res.status(400).json({ message: 'Datanotvalid' });
+
+  res.status(201).json(book);
+};
+
+module.exports = {
+  getAll,
+  getByAuthorId,
+  findById,
+  create,
+};
--- index.js
+++ index.js
@@ -8,45 +8,15 @@ app.use(bodyParser.json());
 const { PORT } = process.env;
 
 const Author = require('./controllers/Author');
-const Book = require('./services/Book');
+const Book = require('./controllers/Book');
 
 app.get('/authors/:id', Author.findById);
 app.get('/authors', Author.getAll);
 app.post('/authors', Author.create);
 
-app.get('/books/author/:author_id', async (req, res) => {
-  const { author_id } = req.params;
-
-  const books = await Book.getByAuthorId(author_id);
-
-  if (!books) return res.status(404).json({ message: 'Books Notfound' });
-
-  res.status(200).json(books);
-});
-
-app.get('/books/:id', async (req, res) => {
-  const { id } = req.params;
-
-  const book = await Book.findById(id);
-
-  if (!book) return res.status(404).json({ message: 'Book Notfound' });
-
-  res.status(200).json(book);
-});
-
-app.get('/books', async (_req, res) => {
-  const books = await Book.getAll();
-
-  res.status(200).json(books);
-});
-
-app.post('/books', async (req, res) => {
-  const { title, author_id } = req.body;
-  const book = await Book.create(title, author_id);
-
-  if (!book) return res.status(400).json({ message: 'Datanotvalid' });
-
-  res.status(201).json(book);
-});
+app.get('/books/author/:author_id', Book.getByAuthorId);
+app.get('/books/:id', Book.findById);
+app.get('/books', Book.getAll);
+app.post('/books', Book.create);
 
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}`); });
