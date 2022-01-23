2247defdc4eac64d23a65d7fe40bf96ed511f232
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 23:23:44 -0300

    Refactor middleware destructuring of validate schema author. query success

--- middlewares/Author.js
+++ middlewares/Author.js
@@ -2,9 +2,9 @@ const { validate } = require('../schemas/Author');
 
 const validateAuthor = (req, res, next) => {
   const { first_name, middle_name, last_name } = req.body;
-  const authorNotValid = validate(first_name, middle_name, last_name);
+  const { code, message } = validate(first_name, middle_name, last_name);
 
-  if (authorNotValid.message) return res.status(authorNotValid.code).json({ message: authorNotValid.message });
+  if (message) return res.status(code).json({ message });
 
   next();
 };
