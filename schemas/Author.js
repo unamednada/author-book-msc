6772ad162302132746cceccffd590fc0f01490bf
unamednada <dias.gustavo.dias@gmail.com>
2022-01-22 22:55:32 -0300

    Create schema of error messages and validation minor functions for books

--- schemas/Author.js
+++ schemas/Author.js
@@ -1,9 +1,20 @@
+const errors = {
+  firstName_blank: 'First name required',
+  fistName_not_string: 'First name must be string',
+  lastName_blank: 'Last name required',
+  lastName_not_string: 'Last name must be string',
+  middleName_not_string: 'Middle name must be string',
+};
+
+const blank = value => !value;
+const notString = value => typeof value !== 'string';
+
 const validate = (firstName, middleName, lastName) => {
-  if (!firstName) return { code: 422, message: 'First name required' };
-  if (typeof firstName !== 'string') return { code: 422, message: 'First name must be string' };
-  if (!lastName) return { code: 422, message: 'Last name required' };
-  if (typeof lastName !== 'string') return { code: 422, message: 'Last name must be string'}
-  if (middleName && typeof middleName !== 'string') return { code: 422, message: 'Middle name must be string' };
+  if (blank(firstName)) return { code: 422, message: errors.firstName_blank };
+  if (notString(firstName)) return { code: 422, message: errors.fistName_not_string  };
+  if (blank(lastName)) return { code: 422, message: errors.lastName_blank };
+  if (notString(lastName)) return { code: 422, message: errors.lastName_not_string }
+  if (middleName && notString(middleName)) return { code: 422, message: errors.middleName_not_string };
   return {};
 };
 
--- schemas/Book.js
+++ schemas/Book.js
@@ -1,10 +1,25 @@
-const validate = async (title, authorId) => {
-  if (!title) return { code: 422, message: 'Title required' };
-  if (typeof title !== 'string') return { code: 422, message: 'Title must be string' };
-  if (!authorId) return { code:422, message: 'Author id required' };
-  
+const Author = require('../models/Author');
+
+const errors = {
+  title_blank: 'Title required',
+  title_not_string: 'Title must be string',
+  authorId_blank: 'Author id required',
+  author_not_found: 'Author Notfound',
+};
+
+const blank = value => !value;
+const notString = value => typeof value !== 'string';
+const authorNotFound = async (authorId) => {
   const author = await Author.findById(authorId);
-  if (!author) return { code: 404, message: 'Author Notfound' };
+  if (!author) return true;
+  return false;
+};
+
+const validate = async (title, authorId) => {
+  if (blank(title)) return { code: 422, message: errors.title_blank };
+  if (notString(title)) return { code: 422, message: errors.title_not_string };
+  if (blank(authorId)) return { code:422, message: errors.authorId_blank };
+  if (await authorNotFound(authorId)) return { code: 404, message: errors.author_not_found };
 
   return {};
 };
